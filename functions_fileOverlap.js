353a43a91cdbb5fc749b7e7a5229e17c4e8b2d4d
Nate Kornell <nkornell@gmail.com>
2020-07-26 10:57:01 -0400

    fixed total article count
    like i said in the title

--- functions_fileOverlap.js
+++ functions_fileOverlap.js
@@ -1,39 +1,19 @@
-//program 1: add to the database
-// If I get access to the api, I might not need to make a database. 
-// just access the api every time. 
-// then again, that wouldn't allow users to check against a known set...
-// but I could have a database that's just known sets. 
-// In this case, I'd want to build in the ability to run batches 
-// (i.e., check out all of the pairs/sets of articles I care about all at once).
-// that might be something I just keep for myself. 
-
-
-// Program 2: crunch the numbers
-// 	Figure out the list of citees that exist in the database. 
-// 	Display a list of citees? Or have them type them in a box
-// 	choose which citees you want to compare
+// 	it would be cool if you could choose which citees you want to compare
 // 		put the originals in one box 
 // 		put the replications in another
-// 	it goes through the database
-// 		figures out spits out how many of each 
-// 		type there are. 
 
-// to do
-// 	each page of html should only do one job. I should split these "apps" up
-//	or maybe both, but there should be separate outputs. 
-// 	and there needs to be a summary output as well. Or maybe only. 
 
 var notesForGoodReferences = "";
 var excludedReferences = "";
 var fileCounter = 0;
-var article = [];
+var reference = [];
 var articleParamNames = [];
 var listOfCitees_combined = [];
 var foOutput = "";
 var stringBetweenArticles = "-&-";
 var reggie = new RegExp(stringBetweenArticles,"g"); // this is a regular expression that can be used to do a replace all
 var year_zero = 3000; // a number greater than the current year
-var	total_citations_all_articles = 0;
+var	total_citations_all = 0;
 
 
 function showFOstuff() {
@@ -41,7 +21,7 @@ function showFOstuff() {
 	x.style.display = "block";
 }
 
-function Article(din, cin, nin, inputArray) {
+function Reference(din, cin, nin, inputArray) {
 	this.dateAdded = din;
 	this.notes = nin;
 	this.citee = cin;
@@ -49,7 +29,8 @@ function Article(din, cin, nin, inputArray) {
 	this.uniqueID = "";
 	var i = 0;
 	for (i = 0; i < inputArray.length; i++ ) {
-		this[articleParamNames[i]] = inputArray[i];
+		this[articleParamNames[i]] = inputArray[i]; // the result of doing this is, for example, reference.DOI = 10.1016/j.jesp.2015.09.017 (this example pretends reference isn't an array). i don't know why it's not reference[DOI] but I guess it's reference.DOI instead for some reason
+// 		console.log( 'apn='+articleParamNames[i]+', inputArray='+ inputArray[i]);
 	}
 }
 
@@ -64,10 +45,9 @@ function add_file_to_FOdatabase() {
 	var tempIDlist = [];
 	var fileNameString = inputCell[0][0].substring(6);
 
-// 	console.log( "i got called");
 	fileCounter++; 
 
-	articleParamNames = inputCell[2]; // dfd for later, check this against expected value
+	articleParamNames = inputCell[2]; // i'm not bothering to check this against expected value but it could be a good idea in case some files were to come out of WOS with a different format
 
 	// Give a warning if the file formatting looks bad. 
 	if (inputCell[0][0].substring(0, 6) != "For:  ") {
@@ -80,7 +60,7 @@ function add_file_to_FOdatabase() {
 			notesForGoodReferences += "<p><strong>"+fileNameString+"</strong> <i>has " + inputCell[i].length + " columns. "+expectedInputArrayLength+" expected.\r\n";
 		}
 		
-		var temp = new Article(d, inputCell[0][0].substring(6), notesForGoodReferences, inputCell[i]);
+		var temp = new Reference(d, inputCell[0][0].substring(6), notesForGoodReferences, inputCell[i]);
 		tempID = temp.Title+". ("+temp["Publication Year"]+"). "+temp.Authors+" "+temp["Source Title"]+". DOI:"+temp.DOI;
 		if (tempIDlist.indexOf(tempID) > -1) {
 			// if it's a repetition
@@ -92,23 +72,11 @@ function add_file_to_FOdatabase() {
 			if (temp["Publication Year"] > 2019) { // dfd this excludes all 2020 citaitons	
 				excludedReferences += "<p><strong>Excluded because year = " + temp["Publication Year"] +"</strong>. In file <i>"+fileNameString+"</i>, excluded the line <i>" + tempID + "</i><br>\r\n";
 			} else {
-				article.push(temp);
-				article[article.length-1].uniqueID = tempID;
+				reference.push(temp);
+				reference[reference.length-1].uniqueID = tempID;
 			}
 		}
 	}
-
-	// next steps (not done yet)
-	// check if the citee/citer pair is already in the database
-	// 	if it is 
-	// 		remove the older version
-	//		add the new one to the database (the new one has newer citation counts)
-	// 		display it on screen with an "updated" label
-	// 	else
-	// 		add it to the database
-	// 		display it on screen with a "new" label
-	// end if
-	
 
 	// if any problems, etc., were found, show them in a box at the bottom. 
 	if ((foFileProblems + notesForGoodReferences + excludedReferences).length > 0) {
@@ -122,9 +90,9 @@ function add_file_to_FOdatabase() {
 function compute_citation_overlap() {
 	// This function is called by file_importExportContent.js
 	
-	// it sorts the article array based on year and uniqueID
+	// it sorts the reference array based on year and uniqueID
 	// I'm pretty sure it's not necessary, but it makes things look nice.
-	article.sort(function(a, b){
+	reference.sort(function(a, b){
 		// sort by publication year from newest to oldest
 		var ya = a['Publication Year'];
 		var yb = b['Publication Year'];
@@ -142,35 +110,34 @@ function compute_citation_overlap() {
 	});
 	
 	// compute min and max year
-	var maxYearAll = Math.max.apply(Math,article.map(function(o){return o["Publication Year"];}));
-	var minYearAll = Math.min.apply(Math,article.map(function(o){return o["Publication Year"];}));
+	var maxYearAll = Math.max.apply(Math,reference.map(function(o){return o["Publication Year"];}));
+	var minYearAll = Math.min.apply(Math,reference.map(function(o){return o["Publication Year"];}));
 	
-	var citee_year_count = []; // dfd make global(?) note: should I have made this part of a listOfCitees_combined object instead of its own thing, maybe. 
+	var citee_year_count = []; // should i make this global? should I have made this part of a listOfCitees_combined object instead of its own thing? maybe. 
 	
-	// Figure out combinations of articles being used. 
-	for (i = 0; i < article.length; i++) {
-		if (i > 0 && article[i-1].uniqueID == article[i].uniqueID) { 
-			// if the citations are the same, remove one of them
-			article[i-1].citee_combined += " " + stringBetweenArticles + " " + article[i].citee;
-			article.splice(i, 1);
+	// if two references are the same, remove one of them 
+	for (i = 0; i < reference.length; i++) {
+		if (i > 0 && reference[i-1].uniqueID == reference[i].uniqueID) { 
+			reference[i-1].citee_combined += " " + stringBetweenArticles + " " + reference[i].citee;
+			reference.splice(i, 1);
 			i--;
 		}
 	}
 
 	// Figure out how often each one is cited per year.
-	for (i = 0; i < article.length; i++) {
-		// console.log( "year = " + article[i]["Publication Year"]);
-		// Figure out the combinations of citee articles that are used. (e.g., a, b, ab).
-		if (listOfCitees_combined.includes(article[i].citee_combined) == false) {
-			listOfCitees_combined.push(article[i].citee_combined); // This only does the ones that are actually used. Should I do possible instead?
+	for (i = 0; i < reference.length; i++) {
+		// console.log( "year = " + reference[i]["Publication Year"]);
+		// Figure out the combinations of citee references that are used. (e.g., a, b, ab).
+		if (listOfCitees_combined.includes(reference[i].citee_combined) == false) {
+			listOfCitees_combined.push(reference[i].citee_combined); // This only does the ones that are actually used. Should I do possible instead?
 		}
-		if (citee_year_count[article[i].citee_combined] == undefined) {
-			citee_year_count[article[i].citee_combined] = [];
+		if (citee_year_count[reference[i].citee_combined] == undefined) {
+			citee_year_count[reference[i].citee_combined] = [];
 		}
-		if (citee_year_count[article[i].citee_combined][article[i]["Publication Year"]] == undefined) {
-			citee_year_count[article[i].citee_combined][article[i]["Publication Year"]] = 0;
+		if (citee_year_count[reference[i].citee_combined][reference[i]["Publication Year"]] == undefined) {
+			citee_year_count[reference[i].citee_combined][reference[i]["Publication Year"]] = 0;
 		}
-		citee_year_count[article[i].citee_combined][article[i]["Publication Year"]]++; // add 1 to how often each one is cited per year. 
+		citee_year_count[reference[i].citee_combined][reference[i]["Publication Year"]]++; // add 1 to how often each one is cited per year. 
 	}
 
 	// Create outputRow (i.e., the number of citation for each row/year). 
@@ -190,7 +157,7 @@ function compute_citation_overlap() {
 
 	// The loop above figures out firstyear. However, it isn't perfect. 
 	// For example, if we have A, B, and A&B, and their firstyears are 1977, 2008, and 1979, newest (B) should 
-	//be 1979, because that's when it was first cited. 
+	// be 1979, because that's when it was first cited. 
 	// The loop above says it's 2008 (the year it was first cited without A). To fix this problem, this loop goes through the whole list
 	// and for each item (i), it checks whether any of the other items in the list (all j) include this item (i) in their string. 
 	// If they do, it sets firstyear equal to the minimum of the years of the two items. 
@@ -205,7 +172,6 @@ function compute_citation_overlap() {
 	
 	// Figure out which citee was sorted first, second, etc. 
 	var foof = [];
-	totalInputArticles = 0;
 	for (i = 0; i < listOfCitees_combined.length; i++) {
 		if (listOfCitees_combined[i].includes("-&-") == false) {
 			foof.push(citee_year_count[listOfCitees_combined[i]].firstYear);
@@ -227,7 +193,6 @@ function compute_citation_overlap() {
 				citee_year_count[listOfCitees_combined[i]].orderOfFirstCitation = "middle";
 				year_zero = Math.min(year_zero, citee_year_count[listOfCitees_combined[i]].firstYear); // set year zero, the year that the first one other than the og came out. 
 			}
-			totalInputArticles++;
 		}
 	}
 
@@ -242,7 +207,7 @@ function compute_citation_overlap() {
 		for (y = minYearAll; y <= maxYearAll; y++) {
 			citee_year_count[listOfCitees_combined[i]].sumAll += citee_year_count[listOfCitees_combined[i]][y];
 		}
-		total_citations_all_articles += citee_year_count[listOfCitees_combined[i]].sumAll;
+		total_citations_all += citee_year_count[listOfCitees_combined[i]].sumAll;
 	}
 
 
@@ -262,7 +227,7 @@ function compute_citation_overlap() {
 // 		foOutput += listOfCitees_combined[i]+ "\t";
 // 		foOutput += citee_year_count[listOfCitees_combined[i]].firstYear +"\t";
 // 		foOutput += citee_year_count[listOfCitees_combined[i]].orderOfFirstCitation +"\t";
-// 		foOutput += totalInputArticles +"\t";
+// 		foOutput += totalCitees +"\t";
 // 		foOutput += uniqueId_of_oldest +"\t";
 // 		foOutput += minYearAll +"\t";
 // 		foOutput += year_zero +"\t";
@@ -275,7 +240,7 @@ function compute_citation_overlap() {
 
 //---------
 	// Create header row for the output in a tab-delimited string.
-	foOutput = "Article(s) Being Cited\tEarliest Citation\tOrder of publication\tNum articles in group\tOldest article\tOldest article's year\tNewest article's year\tTotal years before\tTotal years after\tBefore or after\tYear\t\Year from zero\tCitations\tCitation %\r\n";
+	foOutput = "Article(s) Being Cited\tEarliest Citation\tOrder of publication\tNum files imported\tOldest article\tOldest article's year\tFirst replication year\tTotal years before\tTotal years after\tBefore or after\tYear\t\Year from zero\tCitations\tCitation %\r\n";
 	
 	// Add the rest of the data to the output string
 	for (i = 0; i < listOfCitees_combined.length; i++) {
@@ -283,7 +248,7 @@ function compute_citation_overlap() {
 			foOutput += listOfCitees_combined[i]+ "\t";
 			foOutput += citee_year_count[listOfCitees_combined[i]].firstYear +"\t";
 			foOutput += citee_year_count[listOfCitees_combined[i]].orderOfFirstCitation +"\t";
-			foOutput += totalInputArticles +"\t";
+			foOutput += fileCounter +"\t";
 			foOutput += uniqueId_of_oldest +"\t";
 			foOutput += minYearAll +"\t";
 			foOutput += year_zero +"\t";
@@ -297,7 +262,7 @@ function compute_citation_overlap() {
 			foOutput += y + "\t";
 			foOutput += y - year_zero + "\t";
 			foOutput += citee_year_count[listOfCitees_combined[i]][y] + "\t";
-			foOutput += 100*citee_year_count[listOfCitees_combined[i]][y]/total_citations_all_articles;
+			foOutput += 100*citee_year_count[listOfCitees_combined[i]][y]/total_citations_all;
 			foOutput += "\r\n";
 		}
 	}
@@ -305,7 +270,6 @@ function compute_citation_overlap() {
 //---------
 
 	buildTableFO_numberOfOverlapsEtc(foOutput);
-// 	buildTableFO_listAllCiters();
 
 }
 
@@ -338,80 +302,3 @@ function buildOutputStringFO() {
 	// this is real simple because foOutput is built to be output from square 1. 
 	return foOutput;
 }
-
-
-// function buildTableFO_listAllCiters() {
-// 	// This isn't called anymore. I think it might be for debugging. 
-// 	var out = "";
-// 	var tableName = "foTable_listAllCiters";
-// 
-// 	// Build Header
-// 	out = "<tr>";
-// 	var columnNames = ['Cited By', 'Year', 'Number Cited', 'Article Cited', 'Date Added', 'Notes'];
-// 	for (i = 0; i < columnNames.length; i++) {
-// 		out += "<th onclick='sortTable("+i+", "+'"'+tableName+'"'+")' title='Click to Sort'>"+columnNames[i]+"</th>";
-// 	}
-// 	out += "</tr>";
-// 
-// 	// Build all the other cells.
-// 	var tempCombo = "";
-// 	var tempNumCited = "";
-// 	for (i = 0; i < article.length; i++) {
-// 		tempNumCited = article[i].citee_combined.split(stringBetweenArticles).length;
-// 		tempCombo = article[i].citee_combined;
-// 		if (tempCombo.includes(stringBetweenArticles)) {
-// 			tempCombo = tempCombo.replace(reggie,"<br><div style='color:red'>"+stringBetweenArticles+"</div>")
-// 			// tempCombo = '<td style="text-align: center;" class=cfTableCell_Row_warning>'+tempCombo+'</td>';
-// 			out += "<tr class=cfTableCell_Row_warning>";
-// 		} else {
-// 			// tempCombo= '<td style="text-align: center;">'+tempCombo+'</td>';
-// 			out += "<tr>";
-// 		}
-// 
-// 		// out = out + "<tr>";
-// 		out = out + "<td>" + article[i].uniqueID + "</td>";
-// 		out = out + "<td>" + article[i]["Publication Year"] + "</td>";
-// 		out = out + "<td>" + tempNumCited + "</td>";
-// 		out = out + "<td>" + tempCombo + "</td>";
-// 		out = out + "<td>" + article[i].dateAdded + "</td>";
-// 		out = out + "<td style='color:red'>" + article[i].notes + "</td>";
-// 		out = out + "</tr>";
-// 	}
-// 	
-// 	// Put everything in the HTML on the page.
-// 	var thisTable = document.getElementById(tableName);
-// 	thisTable.innerHTML = "<table>" + out + "</table>";
-// 
-// 	// If necessary, add an alert for excluded files. 
-// 	if (excludedReferences.length > 0) {
-// 		var x = document.getElementById("foProblemBox");
-// 		x.style.display = "block";
-// 		document.getElementById("foProblemText").innerHTML = excludedReferences;
-// 	}
-// }
-
-
-
-
-// ------------------------------------------------------
-// here's a random code snippet
-    // var json = JSON.parse(data),
-	// blob = new Blob([abc], {type: "octet/stream"}),
-	
-// ------------------------------------------------------
-// this is how you get json from a server.
-// If this code is on the server it works. but it doesn't work locally. 
-// Just put it inside <script> tags.
-	// var xmlhttp = new XMLHttpRequest();
-	// var myURL = "EraseMe.txt";
-	
-	// xmlhttp.onreadystatechange = function() {
-	//   if (this.readyState == 4 && this.status == 200) {
-	// 	var myObj = JSON.parse(this.responseText);
-	// //     document.getElementById("demo").innerHTML = myObj.name;
-	// 	console.log( myObj.name);
-	//   }
-	// };
-	
-	// xmlhttp.open("GET", myURL, true);
-	// xmlhttp.send();
