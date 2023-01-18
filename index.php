76499be2fdacee8486eb47ddf4a923c7a9140a12
Nate Kornell <nkornell@gmail.com>
2023-01-18 10:34:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?> 
 
 </body>
 </html>
