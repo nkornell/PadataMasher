a2d359c0c6862665b3e5ff98e7e0c61c225214e1
Nate Kornell <nkornell@gmail.com>
2023-07-22 11:48:16 -0400

    more

--- functions_tables.js
+++ functions_tables.js
@@ -1,12 +1,18 @@
-function sortTable(n,tableName,sortType) {
+function sortTable(n,tableName,sortType,direction) {
 	mostRecentPressedCheckBoxRow = 0; // this is so that after a sort it won't try to press everything spanned by two checkboxes (i.e., it's for something else). 
 
-	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+	var table, rows, i, x, y, shouldSwitch, dir, switchcount = 0;
 	table = document.getElementById(tableName);
-  	switching = true;
+  	var switching = true;
+  	var reversed = false;
+  	
+//   	table.style.cursor = "progress"; // this would be cool but it doesn't refresh while the function runs, so you never see it
 		
   // Set the sorting direction to ascending:
   dir = "asc"; 
+  if (direction != null && direction == "desc") {
+  	dir = "desc"
+  }
   /* Make a loop that will continue until no switching has been done: */
   while (switching) {
 	// Start by saying: no switching is done:
@@ -41,11 +47,6 @@ function sortTable(n,tableName,sortType) {
 	  	console.log( y)
 	  }
 	  
-//		// This figures out whether they're both numbers. Nate added it but I don't need it anymore. Note that this isn't perfect; number will convert the following to numbers: true, false, " ", "", and probably others. 
-// 	  if (isNaN(x) == false && isNaN(y) == false && x.trim() != "" && y.trim != "") {
-// 		sortType == 'numbers';
-//  	  }
-
 	  /* Check if the two rows should switch place, based on the direction, asc or desc: */
 	  if (dir == "asc") {
 		if (x > y) {
@@ -69,15 +70,19 @@ function sortTable(n,tableName,sortType) {
 	  // Each time a switch is done, increase this count by 1:
 	  switchcount ++; 
 	} else {
-	  /* If no switching has been done AND the direction is "asc",
-	  set the direction to "desc" and run the while loop again. */
-	  if (switchcount == 0 && dir == "asc") {
+	  /* If no switching has been done, change the sort direction and run the while loop again. */
+	  if (switchcount == 0 && reversed == false && dir == "asc") {
 		dir = "desc";
 		switching = true;
+		reversed = true;
+	  } else if (switchcount == 0 && reversed == false && dir == "desc") {
+		dir = "asc";
+		switching = true;
+		reversed = true;
 	  }
 	}
   }
-// document.body.style.cursor = 'auto';
+//   table.style.cursor = "auto";
 }
 
 function checkboxOfThisRow(rowIndex,tableName) {
